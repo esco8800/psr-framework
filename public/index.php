1cf3029d45ed4a12dc5a49b1e30b994a3fbf08a0
ilya.semenov <ilya.semenov@chulakov.ru>
2019-12-19 14:22:58 +0300

    логичечкое разделерние индекса

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hello' . PHP_EOL;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+### Init
+
+$request = ServerRequestFactory::fromGlobals();
+
+### Action
+
+$name = $request->getQueryParams()['Name'] ?? 'Guest';
+$response = (new HtmlResponse('Hello ' . $name));
+
+### Sending
+
+echo $response->getBody() . PHP_EOL;
